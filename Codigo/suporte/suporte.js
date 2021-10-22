ccc31516a20a7b1a65e544b2113cff505aab818a
Nikolas Louret <nikoul.ret@gmail.com>
2021-10-21 22:44:40 -0300

    upload responsividade CSS

--- Codigo/suporte/suporte.js
+++ Codigo/suporte/suporte.js
@@ -1,3 +1,12 @@
 onload = () => {
     ethereum.autoRefreshOnNetworkChange = false;
+
 }
+const btnMobile = document.getElementById('btn-mobile');
+
+function tuggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
