ac68c8b95181de41fca9f26959f776519b197c50
Nikolas Louret <nikoul.ret@gmail.com>
2021-12-01 22:01:10 -0300

    [Update] Intregação das páginas 'perfil' e 'Login'

---
+++ Codigo/perfil/js/form.js
@@ -0,0 +1,140 @@
+function validacaoForm() {
+    var asterisco;
+
+    //Verificar os campos do formulário das perguntas individualmente
+    const fields = document.querySelectorAll("[required]");
+
+    function validateField(field) {
+        //Lógica para verificar se existem erros
+        function verifyErrors() {
+            let foundError = false;
+
+            for (key in field.validity) {
+                if (field.validity[key] && !field.validity.valid)
+                    foundError = key;
+            }
+
+            return foundError;
+        }
+
+        function setCustomMessage(message) {
+            if (message) {
+                $(field).attr('placeholder', field.placeholder + message);
+            }
+        }
+
+        return function() {
+            if (verifyErrors()) {
+                field.style.border = "1px solid red";
+
+                if (!asterisco) {
+                    setCustomMessage("*");
+                    asterisco = true;
+                }
+            } else {
+                field.style.border = "1px solid rgb(0, 201, 0)";
+                setCustomMessage();
+            }
+        }
+    }
+
+    function customValidation(event) {
+        const field = event.target;
+        const validation = validateField(field);
+        if (field.placeholder == "Primeiro nome" || field.placeholder == "Adicionar email" || field.placeholder == "Alterar senha")
+            asterisco = false;
+        else asterisco = true;
+        validation();
+    }
+
+    for (field of fields) {
+        field.addEventListener("invalid", event => {
+            //Tirar o bubble
+            event.preventDefault();
+
+            customValidation(event);
+        })
+
+        field.addEventListener("blur", customValidation);
+    }
+}
+
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime(); //Timestamp
+    var d2 = (performance && performance.now && (performance.now() * 1000)) || 0; //Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16; //random number between 0 and 16
+        if (d > 0) { //Use timestamp until depleted
+            r = (d + r) % 16 | 0;
+            d = Math.floor(d / 16);
+        } else { //Use microseconds since page-load if supported
+            r = (d2 + r) % 16 | 0;
+            d2 = Math.floor(d2 / 16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+function salvaPerfil(event) {
+    // Cancela a submissão do formulário para tratar sem fazer refresh da tela
+    event.preventDefault();
+
+    //Faz a verificação individual de cada campo do formulário
+    validacaoForm();
+
+    // // Verfica se o formulário está preenchido corretamente
+    if (!$('#form-configPerfil')[0].checkValidity()) {
+        return;
+    }
+
+    // Obtem os dados do formulário
+    let nome = $("#txt_nome").val();
+    let sobrenome = $("#txt_sobrenome").val();
+    let address = $("#txt_address").val();
+    let email = $("#txt_email").val();
+    let senha = $("#txt_senha").val();
+    let user = {
+        nome: nome,
+        sobrenome: sobrenome,
+        address: address,
+        email: email,
+        senha: senha
+    }
+
+    // Adiciona o usuário no banco de dados
+    updateUser(user);
+}
+
+// Associar salvamento ao botao
+document.getElementById('profile-button').addEventListener('click', salvaPerfil);
+
+// Apaga os dados do usuário corrente no sessionStorage
+function logoutUser() {
+    const perfilCorrente = {};
+    localStorage.setItem('usuarioCorrente', JSON.stringify(perfilCorrente));
+}
+
+const usuario = JSON.parse(localStorage.getItem('usuarioCorrente'));
+const usuariosJSON = JSON.parse(localStorage.getItem('db_usuarios'));
+
+function updateUser(data) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = usuariosJSON.user.map(obj => obj.id).indexOf(usuario.id);
+
+    // Altera os dados do objeto no array
+    usuariosJSON.user[index].nome = data.nome,
+        usuariosJSON.user[index].sobrenome = data.sobrenome,
+        usuariosJSON.user[index].address = data.address,
+        usuariosJSON.user[index].email = data.email,
+        usuariosJSON.user[index].senha = data.senha
+
+    // Atualiza os dados no Local Storage
+    //localStorage.setItem('db_quests', JSON.stringify(db));
+
+    const perfilCorrente = {};
+    localStorage.setItem('usuarioCorrente', JSON.stringify(perfilCorrente));
+
+    // Salva o novo banco de dados com o novo usuário no localStorage
+    localStorage.setItem('db_usuarios', JSON.stringify(usuariosJSON));
+    localStorage.setItem('usuarioCorrente', JSON.stringify(data));
+}
--- Codigo/perfil/js/perfil.js
+++ Codigo/perfil/js/perfil.js
@@ -1,45 +1,45 @@
-// Carregamento dos dados do JSON
-const user = JSON.parse("perfil");
-console.log(user);
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var user = JSON.parse(localStorage.getItem('usuarioCorrente'));
+
+window.addEventListener('load', function() {
+    perfil(user);
+    exibePerfil(user);
+});
 
 
-// function mperfil(data) {
-//     $('#perfil').append(`<!--Imagem do perfil-->
-//                         <img src="${data.avatar_url}" id="avatar_img" title="Foto de Perfil">
+function perfil(data) {
+    $('#perfil').append(`<!--Imagem do perfil-->
+                        <img src="img/perfil-sem-foto.png" id="avatar_img" title="Foto de Perfil">
 
-//                         <!--Texto e Redes do perfil-->
-//                         <div class="infosPerfil">
-//                             <!--Texto-->
-//                             <div class="texto_perfil">
-//                                 <a href="${data.html_url}" target="_blank" title="Perfil no GitHub">${data.name}</a>
-//                                 <p class="bio" title="Biografia">${data.bio}</p>
-//                             </div>
+                        <!--Texto e Redes do perfil-->
+                        <div class="infosPerfil">
+                            <!--Texto-->
+                            <div class="texto_perfil">
+                                <h2 title="Nome de usuário">${data.nome} ${data.sobrenome}</h2>
+                            </div>
 
-//                             <div class="infos">
-//                                 <div class="linha-1">
-//                                     <div id="location">
-//                                         <span><i class="fas fa-map-marker-alt"></i> Localização</span>
-//                                         : ${data.location}
-//                                     </div>
+                            <div class="infos">
+                                <div id="location">
+                                    <span><i class="fas fa-map-marker-alt"></i> Localização</span>
+                                    : ${data.address}
+                                </div>
+                                <div id="email">
+                                    <span><i class="fas fa-envelope"></i> Email</span>
+                                    : ${data.email}
+                                </div>
+                            </div>
+                        </div>`);
+}
 
-//                                     <div id="created_at">
-//                                         <span><i class="fas fa-user"></i> Entrou</span>
-//                                         : ${arrumaData(data.created_at)}
-//                                     </div>
-//                                 </div>
+function exibePerfil(user) {
+    let nome = `${user.nome}`;
+    let sobrenome = `${user.sobrenome}`;
+    let endereco = `${user.endereco}`;
+    let email = `${user.email}`;
 
-//                                 <div class="linha-2">
-//                                     <div id="public_repos">
-//                                         <span><i class="fab fa-github"></i> Repositórios</span>
-//                                         : ${data.public_repos}
-//                                     </div>
-
-//                                     <div id="email">
-//                                         <span><i class="fas fa-envelope"></i> Email</span>
-//                                         : nikoul.ret@gmail.com
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                             <a class="linkPerfil" href="${data.html_url}" target="_blank"><button id="btn-perfil" title="Acessar perfil no GitHub">Carregar perfil</button></a>
-//                         </div>`);
-// }
+    // Substitui as linhas do corpo dos inputs
+    $('#txt_nome').val(nome);
+    $('#txt_sobrenome').val(sobrenome);
+    $('#txt_address').val(endereco);
+    $('#txt_email').val(email);
+}
