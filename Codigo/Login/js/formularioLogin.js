52a134eb037a043128f2fe5227e1c3db43623f62
Nikolas Louret <nikoul.ret@gmail.com>
2021-12-10 17:43:14 -0300

    [Update] Modificação na estruturação do Login

--- Codigo/Login/js/formularioLogin.js
+++ Codigo/Login/js/formularioLogin.js
@@ -1,79 +1,21 @@
 const PERFIL_URL = "https://icei-puc-minas-pples-ti.github.io/PLF-ES-2021-2-TI1-7924100-rotas-gps-1/Codigo/perfil/perfilPrincipal.html";
 var link = JSON.parse(localStorage.getItem('link'));
 
-
-function validacaoForm() {
-    //Verificar os campos do formulário das perguntas individualmente
-    const fields = document.querySelectorAll("[required]");
-
-    function validateField(field) {
-        //Lógica para verificar se existem erros
-        function verifyErrors() {
-            let foundError = false;
-
-            for (key in field.validity) {
-                if (field.validity[key] && !field.validity.valid)
-                    foundError = key;
-            }
-
-            return foundError;
-        }
-
-        function setCustomMessage(message) {
-            if (message) {
-                $(field).attr('placeholder', field.placeholder + message);
-            }
-        }
-
-        return function() {
-            if (verifyErrors()) {
-                field.style.border = "1px solid red";
-
-                if (!asterisco) {
-                    setCustomMessage("*");
-                    asterisco = true;
-                }
-            } else {
-                field.style.border = "1px solid rgb(0, 201, 0)";
-                setCustomMessage();
-            }
-        }
-    }
-
-    function customValidation(event) {
-        const field = event.target;
-        const validation = validateField(field);
-        // if (field.placeholder == "Primeiro nome" || field.placeholder == "Adicionar email" || field.placeholder == "Alterar senha")
-        //     asterisco = false;
-        // else asterisco = true;
-        validation();
-    }
-
-    for (field of fields) {
-        field.addEventListener("invalid", event => {
-            //Tirar o bubble
-            event.preventDefault();
-
-            customValidation(event);
-        })
-
-        field.addEventListener("blur", customValidation);
-    }
-}
-
 // Declara uma função para processar o formulário de login
 function processaFormLogin(event) {
     //Faz a verificação individual de cada campo do formulário
     validacaoForm();
 
     // // Verfica se o formulário está preenchido corretamente
-    if (!$('#login-form1')[0].checkValidity()) {
+    if (!$('#loginForm')[0].checkValidity()) {
+        event.preventDefault();
         return;
     }
 
     // Obtem os dados de login e senha a partir do formulário de login
     var username = $('#username').val();
     var password = $('#password').val();
+    console.log(username)
 
     // Valida login e se estiver ok, redireciona para tela inicial da aplicação
     if (username && password) {
@@ -83,8 +25,7 @@ function processaFormLogin(event) {
             // Cancela a submissão do formulário para tratar sem fazer refresh da tela
             event.preventDefault();
             alert('Usuário ou senha incorretos');
-        } else
-            window.location.href = link;
+        }
     }
 }
 
@@ -97,43 +38,20 @@ function linkBtnEntrar() {
         inputSenha.oninput = () => {
             if (loginUser(inputNome.value, inputSenha.value))
                 btnEntrar.setAttribute('href', link);
+            else
+                btnEntrar.setAttribute('href', "");
         }
 }
 
-function salvaLogin(event) {
-    //Faz a verificação individual de cada campo do formulário
-    validacaoForm();
-
-    // // Verfica se o formulário está preenchido corretamente
-    if (!$('#login-form2')[0].checkValidity()) {
-        return;
-    }
-
-    // Obtem os dados do formulário
-    let nome = document.getElementById('txt_nome').value;
-    let sobrenome = document.getElementById('txt_sobrenome').value;
-    let email = document.getElementById('txt_email').value;
-    let senha = document.getElementById('txt_senha').value;
-
-    // Adiciona o usuário no banco de dados
-    addUser(nome, sobrenome, senha, email);
-    alert('Usuário salvo com sucesso. Proceda com o login para ');
-
-    // Oculta a div modal do login
-    //document.getElementById ('loginModal').style.display = 'none';
-    $('#loginModal').modal('hide');
-}
-
-// Associa a funçao processaFormLogin  formulário adicionado um manipulador do evento submit
-document.getElementById('login-form1').addEventListener('submit', processaFormLogin);
-
-
-// Associar salvamento ao botao
-document.getElementById('btn_salvar').addEventListener('click', salvaLogin);
-
-function start() {
+$(document).ready(function() {
     const btnEntrar = document.querySelector('#btn-entrar');
 
     if (link == '' || link == null)
         btnEntrar.setAttribute('href', "https://icei-puc-minas-pples-ti.github.io/PLF-ES-2021-2-TI1-7924100-rotas-gps-1/Codigo/rotas/rotas.html");
-}
+
+    // Associa a funçao processaFormLogin  formulário adicionado um manipulador do evento submit
+    document.getElementById('btnEntrar').addEventListener('click', processaFormLogin);
+
+    // Associar link da página antiga ao botão entrar
+    document.getElementById('password').addEventListener('focus', linkBtnEntrar);
+})
---
+++ Codigo/Login/js/formularioNewUser.js
@@ -0,0 +1,48 @@
+var link = JSON.parse(localStorage.getItem('link'));
+
+function salvaLogin(event) {
+    //Faz a verificação individual de cada campo do formulário
+    validacaoForm();
+
+    // // Verfica se o formulário está preenchido corretamente
+    if (!$('#newUserForm')[0].checkValidity()) {
+        event.preventDefault();
+        return;
+    }
+
+    // Obtem os dados do formulário
+    let nome = document.getElementById('txt_nome').value;
+    let sobrenome = document.getElementById('txt_sobrenome').value;
+    let email = document.getElementById('txt_email').value;
+    let senha = document.getElementById('txt_senha').value;
+
+    // Adiciona o usuário no banco de dados
+    addUser(nome, sobrenome, senha, email);
+}
+
+function linkBtnEntrar() {
+    const btnSalvar = document.querySelector('#salvarLink');
+    const inputSenha1 = document.querySelector('#txt_senha');
+    const inputSenha2 = document.querySelector('#txt_senha2');
+
+    if (link)
+        inputSenha2.oninput = () => {
+            if (inputSenha1.value == inputSenha2.value)
+                btnSalvar.setAttribute('href', link);
+            else
+                btnSalvar.setAttribute('href', "");
+        }
+}
+
+$(document).ready(function() {
+    const btnSalvar = document.querySelector('#salvarLink');
+
+    if (link == '' || link == null)
+        btnSalvar.setAttribute('href', "https://icei-puc-minas-pples-ti.github.io/PLF-ES-2021-2-TI1-7924100-rotas-gps-1/Codigo/rotas/rotas.html");
+
+    // Associa a funçao processaFormLogin  formulário adicionado um manipulador do evento submit
+    document.getElementById('btn_salvar').addEventListener('click', salvaLogin);
+
+    // Associar link da página antiga ao botão entrar
+    document.getElementById('txt_senha2').addEventListener('focus', linkBtnEntrar);
+})
