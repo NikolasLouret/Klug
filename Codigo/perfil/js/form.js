b3c703ac079e3f7f1ec5aae87fffd13d78658a88
Igor Martins <iguinhoigor4@gmail.com>
2021-12-13 14:18:28 -0300

    fix: Consertar coisas

--- Codigo/perfil/js/form.js
+++ Codigo/perfil/js/form.js
@@ -228,9 +228,7 @@ function updateSenha(data) {
         usuariosJSON.user[index].sobrenome = data.sobrenome,
         usuariosJSON.user[index].address = data.address,
         usuariosJSON.user[index].email = data.email,
-        usuariosJSON.user[index].senha = data.senha,
-        usuariosJSON.user[index].foto = data.foto,
-        usuariosJSON.user[index].pontos = data.pontos;
+        usuariosJSON.user[index].senha = data.senha
 
     const perfilCorrente = {};
     localStorage.setItem('usuarioCorrente', JSON.stringify(perfilCorrente));
