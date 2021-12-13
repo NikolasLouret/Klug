52c45f983df8e813340051f6e166c0abd87ee109
Igor Martins <iguinhoigor4@gmail.com>
2021-12-13 12:28:19 -0300

    fix: Mudar link do botão de login página suporte

--- Codigo/suporte/pagina-inicial/js/app.js
+++ Codigo/suporte/pagina-inicial/js/app.js
@@ -182,6 +182,6 @@ function init() {
     })
 
     login.addEventListener('click', function() {
-        localStorage.setItem('link', JSON.stringify("https://icei-puc-minas-pples-ti.github.io/PLF-ES-2021-2-TI1-7924100-rotas-gps-1/Codigo/suporte/perguntas/perguntas.html"));
+        localStorage.setItem('link', JSON.stringify("../../suporte/perguntas/perguntas.html"));
     })
 }
